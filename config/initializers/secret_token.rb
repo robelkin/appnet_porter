0482bdca5bf05a7c5d2f57ca3af922fc79b88636
Rob Elkin <robelkin@me.com>
2012-08-11 14:13:27 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppnetPorter::Application.config.secret_token = '7fa23b4cbc4b666c2109ad3abb14cb82c82b4ffb61fab7d9fd5298af953f14ec61e7cffade2d1c4aa85b75889e0c3479644266ca577e405a78fd3aef9cd4526e'
